2650dd40cc538e8b221be97f99625e466b12e43f
SylvaticGhost <143171907+SylvaticGhost@users.noreply.github.com>
2024-02-07 13:58:47 +0200

    f

---
+++ Lab1/MainController.cs
@@ -0,0 +1,21 @@
+﻿using System.Text.Json;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace Lab1;
+
+[ApiController]
+[Route("[controller]")]
+public class MainController : ControllerBase
+{
+   public MainController() {}
+   
+   [HttpPut("CountZero")]
+   public int CountZeroInMatrix(Matrix? matrix)
+   {
+      if(matrix is not null)
+         return Functions.CountZeroInMatrix(matrix.Containing);
+
+      throw new BadHttpRequestException("Empty input");
+   }
+}
