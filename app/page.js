a54e0254e537f5a8d2c528543b4a8db7b228b47e
layron <53446309+layron11@users.noreply.github.com>
2024-02-19 20:21:26 +0200

    Update page.js

--- app/page.js
+++ app/page.js
@@ -25,7 +25,7 @@ export default function Home() {
       return matrix
     }
   }
-  
+
   function checkValidSize(n, m){
     if(n.isEmpty || m.isEmpty)
     {
@@ -34,42 +34,53 @@ export default function Home() {
     }
     else
     {
-      if (!Number.isInteger(n)) 
+      if (!Number.isInteger(n))
       {
         alert("number of rows aren't an integer");
         return false;
-      } 
-      else if (!Number.isInteger(m)) 
+      }
+      else if (!Number.isInteger(m))
       {
         alert("number of columns aren't an integer")
         return false;
       }
-      else 
+      else
         return true;
     }
   }
 
   function readFile(event) {
     const file = event.target.files[0];
-    if(CheckFormatOfFile(file.name))
+    if(checkFormatOfFile(file.name))
     {
       const reader = new FileReader();
       reader.onload = function (event) {
-        setMatrix(event.target.result)
+        if (checkDataFile(event.target.result)) {
+          setMatrix(event.target.result)
+        } else {
+          alert("File don't contains matrix")
+        }
       };
 
       reader.readAsText(file);
     }
   }
-  
-  function CheckFormatOfFile(fileName) {
-    const allowedExtensions = [".txt", ".csv"];
+
+  function checkDataFile(val) {
+    const pattern1 = /^\d\s\n+$/;
+    const pattern2 = /\s{2,}/;
+
+    return pattern1.test(val) && !pattern2.test(val);
+  }
+
+  function checkFormatOfFile(fileName) {
+    const allowedExtensions = ["txt", "csv"];
     const extension = fileName.split('.').pop().toLowerCase();
     if (!allowedExtensions.includes(extension)) {
       alert("Invalid file format!");
       return false;
     }
-    
+
     return true;
   }
 
@@ -96,17 +107,17 @@ export default function Home() {
   }
 
   function validationTextArea(e) {
-    const regex = /^[\d\s\n]*$/;
+    const regex = /^[\d\s\n]+$/;
     const input = e.target.value;
 
     if (!regex.test(input)) {
-      e.target.value = input.replace(/[^\d\s\n]/g, ''); 
+      e.target.value = input.replace(/[^\d\s\n]/g, '');
     }
   }
 
   function HttpMessage(arr) {
     CheckInputMatrixInSize(arr, Number(rows), Number(columns))
-    
+
     axios.post(URL + "/Main/CountZero", {
       Containing: arr
     }, {
@@ -116,21 +127,21 @@ export default function Home() {
         'AcceptEncoding' : 'gzip, deflate, br',
       }
     })
-        .then(function (response) {
-          setResult(String(response.data))
-        })
-        .catch(function (error) {
-          console.log(error)
-          if(error.response.status === 400)
-            alert("Incorrect Input, you should check if input contains only numbers\n or invalid size")
-          else {
-            const msg = error.response
-            alert(error.response)
-          }
-        });
+      .then(function (response) {
+        setResult(String(response.data))
+      })
+      .catch(function (error) {
+        console.log(error)
+        if(error.response.status === 400)
+          alert("Incorrect Input, you should check if input contains only numbers\n or invalid size")
+        else {
+          const msg = error.response
+          alert(error.response)
+        }
+      });
   }
-  
-  
+
+
   function CheckInputMatrixInSize(matrix, n, m) {
     checkValidSize(n, m);
     const rowSize = matrix[0].length;
@@ -140,7 +151,7 @@ export default function Home() {
         return false;
       }
     }
-    
+
     if(Number(matrix.length) !== Number(n)) {
       alert("the number of rows in box and on the filed aren't the same\n Check number of row");
       return false;
@@ -149,111 +160,113 @@ export default function Home() {
       alert("Number of columns aren't the same in the box and on the field\n Check number of columns");
       return false;
     }
-    
+
     return true;
   }
 
   return (
-      <>
-        <main className="flex flex-row gap-7 justify-center p-10">
-          
-          <div id="area" className='flex-auto w-30'>
-            <h1 className='mb-10'>Fill in the matrix</h1>
+    <>
+      <main className="flex flex-row gap-7 justify-center p-10">
 
-            <textarea id="message"
-                      className="block p-2.5 w-full h-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500"
-                      placeholder="Fill in the matrix using a space as a separator between elements and a line break as a separator between rows"
-                      value={matrix} onKeyUp={e => validationTextArea(e)} onChange={(e) => {
-              setMatrix(e.target.value);
-              setResult("-");
-            }}></textarea>
+        <div id="area" className='flex-auto w-30'>
+          <h1 className='mb-10'>Fill in the matrix</h1>
 
-          </div>
-          <div id="random_box " className='flex-auto w-30'>
-            <h2>OR Generate random matrix</h2>
-            <form className="max-w-sm mx-auto mt-10">
+          <textarea id="message"
+                    className="block p-2.5 w-full h-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500"
+                    placeholder="Fill in the matrix using a space as a separator between elements and a line break as a separator between rows"
+                    value={matrix} onKeyUp={e => validationTextArea(e)} onChange={(e) => {
+            setMatrix(e.target.value);
+            setResult("-");
+          }}></textarea>
 
-              <label htmlFor="number-input" className="block mb-2 text-sm font-medium text-gray-900 ">Count of
-                rows:</label>
-              <input type="number" id="number-input" aria-describedby="helper-text-explanation"
-                     className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 
+        </div>
+        <div id="random_box " className='flex-auto w-30'>
+          <h2>OR Generate random matrix</h2>
+          <form className="max-w-sm mx-auto mt-10">
+
+            <label htmlFor="number-input" className="block mb-2 text-sm font-medium text-gray-900 ">Count of
+              rows:</label>
+            <input type="number" id="number-input" aria-describedby="helper-text-explanation"
+                   className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500
                      focus:border-blue-500 block w-100 p-2.5 " min={1}
-                     placeholder="" value={rows} onChange={(event) => {
-                setRows(event.target.value)
-              }}/>
-              <label htmlFor="number-input" className="block mt-3 mb-2 text-sm font-medium text-gray-900 ">Count of
-                columns:</label>
-              <input type="number" id="number-input" aria-describedby="helper-text-explanation"
-                     className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-100 p-2.5 "
-                     placeholder="" value={columns} onChange={(event) => {
-                setColumns(event.target.value)
-              }}/>
+                   placeholder="" value={rows} onChange={(event) => {
+              setRows(event.target.value)
+            }}/>
+            <label htmlFor="number-input" className="block mt-3 mb-2 text-sm font-medium text-gray-900 ">Count of
+              columns:</label>
+            <input type="number" id="number-input" aria-describedby="helper-text-explanation"
+                   className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-100 p-2.5 "
+                   placeholder="" value={columns} onChange={(event) => {
+              setColumns(event.target.value)
+            }}/>
 
-              <button type="button" onClick={() => {
-                generateMatrix(rows, columns)
-              }}
-                      className="text-white mt-10 bg-gradient-to-r from-purple-500 via-purple-600 to-purple-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-purple-300 dark:focus:ring-purple-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Generate
-              </button>
+            <button type="button" onClick={() => {
+              generateMatrix(rows, columns)
+            }}
+                    className="text-white mt-10 bg-gradient-to-r from-purple-500 via-purple-600 to-purple-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-purple-300 dark:focus:ring-purple-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Generate
+            </button>
 
-            </form>
+          </form>
 
-          </div>
-          <div id="drag_n" className='flex-auto w-30'>
-            <h2 className='pb-10'>OR Download a data file</h2>
-            <div className="w-[300px] relative border-2 border-gray-300 border-dashed rounded-lg p-6" id="dropzone">
-              <input type="file" className="absolute inset-0 w-full h-full opacity-0 z-50" onChange={
-                (e) => {
-                  readFile(e)
-                }
-              }/>
-              <div className="text-center">
-                <img className="mx-auto h-12 w-12" src="https://www.svgrepo.com/show/389177/file-input.svg" alt=""/>
+        </div>
+        <div id="drag_n" className='flex-auto w-30'>
+          <h2 className='pb-10'>OR Download a data file</h2>
+          <div className="w-[300px] relative border-2 border-gray-300 border-dashed rounded-lg p-6" id="dropzone">
+            <input type="file" className="absolute inset-0 w-full h-full opacity-0 z-50" onChange={
+              (e) => {
+                readFile(e)
+              }
+            }/>
+            <div className="text-center">
+              <img className="mx-auto h-12 w-12" src="https://www.svgrepo.com/show/389177/file-input.svg" alt=""/>
 
-                <h3 className="mt-2 text-sm font-medium text-gray-900">
-                  <label htmlFor="file-upload" className="relative cursor-pointer">
-                    <span>Drag and drop</span>
-                    <span className="text-indigo-600"> or browse </span>
-                    <span>to upload</span>
-                    <input id="file-upload" name="file-upload" type="file" className="sr-only" onChange={
-                      (e) => {
-                        readFile(e)
-                      }
-                    }/>
-                  </label>
-                </h3>
-                <p className="mt-1 text-xs text-gray-500">
-                  TXT Files
-                </p>
-              </div>
-
-              <img src="" className="mt-4 mx-auto max-h-40 hidden" id="preview" alt={'jj'}/>
+              <h3 className="mt-2 text-sm font-medium text-gray-900">
+                <label htmlFor="file-upload" className="relative cursor-pointer">
+                  <span>Drag and drop</span>
+                  <span className="text-indigo-600"> or browse </span>
+                  <span>to upload</span>
+                  <input id="file-upload" name="file-upload" type="file" className="sr-only" onChange={
+                    (e) => {
+                      readFile(e)
+                    }
+                  }/>
+                </label>
+              </h3>
+              <p className="mt-1 text-xs text-gray-500">
+                TXT Files
+              </p>
             </div>
+
+            <img src="" className="mt-4 mx-auto max-h-40 hidden" id="preview" alt={'jj'}/>
           </div>
-        </main>
-        <div className="output flex justify-center pt-20">
-          <button type="button"
-                  className="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2"
-                  onClick={() => {
-                    setMatrix('');
-                    setRows(4)
-                    setColumns(4)
-                  }}>Reset
-          </button>
-          <button
-              className="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-cyan-500 to-blue-500 group-hover:from-cyan-500 group-hover:to-blue-500 hover:text-white focus:ring-4 focus:outline-none focus:ring-cyan-200 "
-              onClick={() => {
-                sendData()
-              }}>
+        </div>
+      </main>
+      <div className="output flex justify-center pt-20">
+        <button type="button"
+                className="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2"
+                onClick={() => {
+                  setMatrix('');
+                  setRows(4)
+                  setColumns(4)
+                }}>Reset
+        </button>
+        <button
+          className="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-cyan-500 to-blue-500 group-hover:from-cyan-500 group-hover:to-blue-500 hover:text-white focus:ring-4 focus:outline-none focus:ring-cyan-200 "
+          onClick={() => {
+            sendData()
+          }}>
               <span
-                  className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white rounded-md group-hover:bg-opacity-0">
+                className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white rounded-md group-hover:bg-opacity-0">
               Send
               </span>
-          </button>
-        </div>
-        <div className="p-4 mb-4 mt-5 ml-10 w-96 text-sm text-green-800 rounded-lg bg-green-50 "
-             role="alert">
-          <span className="font-medium text-base">Count of zeros: </span> {result}
-        </div>
-      </>
+        </button>
+      </div>
+      <div className="p-4 mb-4 mt-5 ml-10 w-96 text-sm text-green-800 rounded-lg bg-green-50 "
+           role="alert">
+        <span className="font-medium text-base">Count of zeros: </span> {result}
+      </div>
+
+      <h1 className='text-center text-xl'>Бригада №5. Лабораторна робота №1</h1>
+    </>
   );
 }
